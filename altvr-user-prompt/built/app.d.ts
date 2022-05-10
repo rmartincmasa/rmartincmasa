217c588c291f08ea78ada74c079621ba15193c0a
rmartincmasa <105300372+rmartincmasa@users.noreply.github.com>
2022-05-10 17:00:04 +0200

    First AltVR commit
    Development files

---
+++ altvr-user-prompt/built/app.d.ts
@@ -0,0 +1,23 @@
+import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+export default class EmailCapture {
+    private context;
+    private baseUrl;
+    private assets;
+    private userInput;
+    private DEBUG;
+    private EmailList;
+    private buttonMaterial;
+    private position;
+    private rotation;
+    private buttonMesh;
+    private buttonActor;
+    private labelActor;
+    private labelText;
+    /**
+     * Context Constructor
+     */
+    constructor(context: MRE.Context, baseUrl: string);
+    private signUp;
+    private init;
+}
+//# sourceMappingURL=app.d.ts.map
